73ac5b9850f1cff91dad43f8ec11419506ff9769
EduardoAtene <eduardoatenesilvae@gmail.com>
2021-05-22 16:57:45 -0300

    Fazendo algumas alterações

---
+++ js/funcao.js
@@ -0,0 +1,8 @@
+/*window.onscroll = scrollHeader();
+function scrollHeader(){
+    if(document.body.scrollTop >50 || document.documentElement.scrollTop >50){
+        document.getElementById("header-conteiner").className ="conteiner-fixed";
+    }else{
+        document.getElementById("header-conteiner").className ="conteiner";
+    }
+} */
